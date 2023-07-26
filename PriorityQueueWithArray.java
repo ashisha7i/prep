a5bb1f7c2d85009d8e43fbfe208cafdce0e8dafe
Ashish <agnihotri.ashish@gmail.com>
2023-07-26 02:02:38 -0700

    Added new solution

--- PriorityQueueWithArray.java
+++ PriorityQueueWithArray.java
@@ -1,4 +1,4 @@
-public class PriorityQueueDS {
+public class PriorityQueueWithArray {
     public static void main(String[] args) {
         MyPriorityQueue mpq = new MyPriorityQueue(10);
         mpq.insert(1);
